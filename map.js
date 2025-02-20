9c3b4cdd090f4e9710b7a452a52ea52bec069147
banffjiang <banffjiang327@gmail.com>
2025-02-19 16:22:19 -0800

    step 3 4

--- map.js
+++ map.js
@@ -14,31 +14,124 @@
      maxZoom: 18 // Maximum allowed zoom
    });
 
-   map.on('load', () => { 
-    //code 
-  });
+   map.on('load', () => {
+    const svg = d3.select('#map').select('svg');
+    let stations = [];
+    let circles;
+
+    function getCoords(station) {
+        const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+        const { x, y } = map.project(point);
+        return { cx: x, cy: y };
+    }
+
+    function updatePositions() {
+        circles
+            .attr('cx', d => getCoords(d).cx)
+            .attr('cy', d => getCoords(d).cy);
+    }
+
+    const tooltip = d3.select("body").append("div")
+        .attr("id", "tooltip")
+        .style("position", "absolute")
+        .style("background", "rgba(0,0,0,0.7)")
+        .style("color", "#fff")
+        .style("padding", "5px 10px")
+        .style("border-radius", "5px")
+        .style("display", "none");
+
+    const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+    d3.json(jsonurl).then(jsonData => {
+        stations = jsonData.data.stations;
+
+        circles = svg.selectAll('circle')
+            .data(stations)
+            .enter()
+            .append('circle')
+            .attr('r', 5)
+            .attr('fill', 'steelblue')
+            .attr('stroke', 'white')
+            .attr('stroke-width', 1)
+            .attr('opacity', 0.8);
+
+        updatePositions();
+
+        const trafficUrl = 'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv';
+        d3.csv(trafficUrl).then(data => {
+            trips = data;
+
+            let departures = d3.rollup(
+                trips,
+                v => v.length,
+                d => d.start_station_id
+            );
+
+            let arrivals = d3.rollup(
+                trips,
+                v => v.length,
+                d => d.end_station_id
+            );
+
+            stations = stations.map(station => {
+                let id = station.short_name;
+                station.arrivals = arrivals.get(id) ?? 0;
+                station.departures = departures.get(id) ?? 0;
+                station.totalTraffic = station.arrivals + station.departures;
+                return station;
+            });
+
+            radiusScale = d3
+                .scaleSqrt()
+                .domain([0, d3.max(stations, d => d.totalTraffic)])
+                .range([0, 25]);
+
+            circles
+                .data(stations)
+                .transition()
+                .duration(500)
+                .attr('r', d => radiusScale(d.totalTraffic));
+
+            circles.on("mouseover", (event, d) => {
+                tooltip.style("display", "block")
+                    .html(`${d.totalTraffic} Trips (${d.departures} Departures, ${d.arrivals} Arrivals)`)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY + 10) + "px");
+            })
+            .on("mousemove", (event) => {
+                tooltip.style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY + 10) + "px");
+            })
+            .on("mouseout", () => {
+                tooltip.style("display", "none");
+            });
+
+        }).catch(error => {
+            console.error('Error loading traffic data:', error);
+        });
+    }).catch(error => {
+        console.error('Error loading JSON:', error);
+    });
 
     map.addSource('boston_route', {
         type: 'geojson',
-        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
-      });
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
 
-      map.addLayer({
-        id: 'bike-lanes',
-        type: 'line',
-        source: 'boston_route',
-        paint: {
-          'line-color': '#32D400',
-          'line-width': 5,
-          'line-opacity': 0.6
-        }
-      });
-
-      map.addSource('cambridge_route', {
+    map.addSource('cambridge_route', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
     });
 
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',
+            'line-width': 3,
+            'line-opacity': 0.6
+        }
+    });
 
     map.addLayer({
         id: 'bike-lanes-cambridge',
@@ -51,3 +144,8 @@
         }
     });
 
+    map.on('move', updatePositions);
+    map.on('zoom', updatePositions);
+    map.on('resize', updatePositions);
+    map.on('moveend', updatePositions);
+});
